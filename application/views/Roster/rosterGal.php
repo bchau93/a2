caf40db38454cae96d71d4972407e0f2670133dc
Julia <jyiu93@gmail.com>
2015-11-15 19:18:34 -0800

    routes change, roster pages added css

--- application/views/Roster/rosterGal.php
+++ application/views/Roster/rosterGal.php
@@ -1,28 +1,76 @@
-<div id="toggles">
+<div class="row">
+    <div class="text">
+        <div id="toggles">
             <div id="orderToggles">
                 <label>
-                    <input type="radio" name="order" value="playerLastName" onchange="toggle(this)" {nameDefault}>Name</input>
+                    <input type="radio" 
+                           name="order" 
+                           value="playerLastName" 
+                           onchange="toggle(this)" {nameDefault}>
+                        Name
+                    </input>
                 </label>
                 <label>
-                    <input type="radio" name="order" value="playerPosition" onchange="toggle(this)" {posDefault}>Position</input>
+                    <input type="radio" 
+                           name="order" 
+                           value="playerPosition" 
+                           onchange="toggle(this)" {posDefault}>
+                        Position
+                    </input>
                 </label>
                 <label>
-                    <input type="radio" name="order" value="playerNumber" onchange="toggle(this)" {numDefault}>Jersey #</input>
+                    <input type="radio" 
+                           name="order" 
+                           value="playerNumber" 
+                           onchange="toggle(this)" {numDefault}>
+                        Jersey #
+                    </input>
                 </label>
             </div>
 
             <div id="layoutToggles">
                 <label>
-                    <input type="radio" name="layout" value="table" onchange="toggle(this)" {tabDefault}>Table</input>
+                    <input type="radio" 
+                           name="layout" 
+                           value="table" 
+                           onchange="toggle(this)" {tabDefault}>
+                        Table
+                    </input>
                 </label>
                 <label>
-                    <input type="radio" name="layout" value="gallery" onchange="toggle(this)" {galDefault}>Gallery</input>
+                    <input type="radio" 
+                           name="layout" 
+                           value="gallery" 
+                           onchange="toggle(this)" {galDefault}>    
+                        Gallery
+                    </input>
                 </label>
             </div>
-    
+
             <div id="editToggle">
                 <label>
-                    <input type="checkbox" name="edit" value="edit" onchange="toggleEdit(this)" {editDefault}>Edit Players</input>
+                    <input type="checkbox" 
+                           name="edit" 
+                           value="edit" 
+                           onchange="toggleEdit(this)" {editDefault}>
+                        Edit Players
+                    </input>
                 </label>
             </div>
-        </div
+        </div>
+
+        <h2>The Bengals Players</h2>
+        <br/>
+        <table cols="3">
+            {players}    
+            <a href="/Roster/playerDetails/{id}" title="{playerFirstName} {playerLastName} - {playerNumber}">
+                <img src="/assets/images/players/{playerPhoto}"  width="200px" height="150px"/>  
+            </a> 
+            {/players}
+        </table>
+    </div>
+    
+    <div class="pagination-centered">
+        {pagination}
+    </div>
+</div>
